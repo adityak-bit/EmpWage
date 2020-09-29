c8932825509ab9e11c0745e223be0f28cb5d5128
adityak-bit <adityajsr1998@gmail.com>
2020-09-29 07:53:49 +0530

    Added : Part time employee and wage

--- Emp/src/com/Capgemini/EmpWageBuilder.java
+++ Emp/src/com/Capgemini/EmpWageBuilder.java
@@ -3,19 +3,24 @@ package com.Capgemini;
 import java.util.Random;
 
 public class EmpWageBuilder {
-	
-		public static final int IS_FULL_TIME = 1;
-		
-		public static void main(String[] args) {
-	     
-			Random r = new Random();
-	     int empCheck =  r.nextInt(2) ;
-	     
-	     if(empCheck == IS_FULL_TIME)
-	             System.out.println("Employee is Present");
-	     else
-	    	     System.out.println("Employee is Abscent");
-		}
 
+	public static final int IS_FULL_TIME = 1;
+	public static final int EMP_RATE_PER_HOUR = 20;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int empHrs = 0, empWage = 0;
+
+		Random r = new Random();
+		int empCheck = r.nextInt(2);
+
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 
+}
